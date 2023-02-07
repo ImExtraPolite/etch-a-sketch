cbc3a3c5537cf0c99f07247bebe1bd7099552cd0
ImExtraPolite <jaleel1666@gmail.com>
2023-02-07 09:31:55 -0500

    make rows

--- javascript.js
+++ javascript.js
@@ -1,7 +1,13 @@
-for (let i = 1; i <= 256; i++) {
-  const container = document.querySelector("#container");
-  const boxes = document.createElement("div");
+// for (let i = 1; i <= 16; i++) {
+//   for(let j = 1; j < 16; j++) {
+//     const container = document.querySelector("#container");
+//     const boxes = document.createElement("div");
+  
+//     container.appendChild(boxes);
+//   }
+//   const container = document.querySelector("#container");
+//   const newBoxes = document.createElement("div");
 
-  container.appendChild(boxes);
-}
+//   container.appendChild(newBoxes);
+// }
 
