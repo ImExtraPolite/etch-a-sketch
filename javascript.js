e92b370a9eb3d716a29a6c2d14fe192bf0a1b6f9
ImExtraPolite <jaleel1666@gmail.com>
2023-02-09 07:12:34 -0500

    make grid

--- javascript.js
+++ javascript.js
@@ -12,13 +12,29 @@ columns();
 
 function rows() {
   for (let i = 1; i <= 16; i++) {
-    i.toString;
-    const columns = document.querySelector(".row" + i);
-    const rows = document.createElement("div");
+    for(let j = 1; j <= 16; j++) {
+      j.toString;
+      const columns = document.querySelector(".row" + i);
+      const rows = document.createElement("div");
+      rows.style.border = "solid";
+      rows.classList.add("squares");
 
-    columns.appendChild(rows);
+      columns.appendChild(rows);
+    }
   }
 }
 rows();
+
+for(let i = 1; i <= 16; i++) {
+  i.toString;
+  const rowOne = document.querySelector(".row" + i);
+  rowOne.style.padding = 0;
+  rowOne.style.margin = 0;
+}
+
+
+
+
+
 
 
